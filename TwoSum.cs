86c546693942fee4769d1f009eda799ab08b2181
Hoang <phamhoang15032005ii@gmail.com>
2025-03-24 19:22:56 +0700

    Add files via upload

---
+++ TwoSum.cs
@@ -0,0 +1,19 @@
+﻿
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> mang = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length;i++)
+        {
+            int tg = target - nums[i];
+            if(mang.ContainsKey(tg))
+            {
+                return new int[] {mang[tg],i};
+            }
+            mang[nums[i]] = i;
+        }
+        return new int[] {0,0};
+    }
+} 
